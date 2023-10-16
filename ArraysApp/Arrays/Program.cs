1c9f16a323640432680d3fbc2f7cc92a955eb665
surrenderat20 <lassevfuglsbjerg@gmail.com>
2023-10-16 16:16:12 +0200

    arrays practice

--- ArraysApp/Arrays/Program.cs
+++ ArraysApp/Arrays/Program.cs
@@ -21,7 +21,22 @@
 //single quote identifies a char
 //double identifies a string
 
+
 string data = "bob,sue,hanes,mark,bobby";
 string[] firstNames = data.Split(',');
 
-Console.WriteLine($"{firstNames[3]}");
+Console.WriteLine($"{firstNames[1]}");
+
+Console.WriteLine(firstNames.Length);
+
+string[] lastNames = new string[] { "corey", "smith", "jones" };
+int[] ages = new int[] {2,3,4};
+
+
+string[] brothers = new string[] { "Morten, Mikkel" };
+int[] brothersAge = new int[] {23, 33};
+
+string friendsdata = "jonathan, niklas, ludvig, teis, oscar, kasper";
+string[] friends = friendsdata.Split(",");
+
+Console.WriteLine($"{friends[2]}");
